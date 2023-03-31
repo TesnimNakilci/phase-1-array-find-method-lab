5405f7908f7fc07b598be0d49175779e09aeda22
TesnimNakilci <115715881+TesnimNakilci@users.noreply.github.com>
2023-03-31 17:48:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+/*const testV = {}
+function testFunc() {
+  return "helloo"
+}*/
+
+function superbowlWin(objArr) {
+  const winYear = objArr.find( e => e.result === "W")
+  if(winYear) {
+    return winYear.year
+  } else {
+    return undefined
+  }
+}
